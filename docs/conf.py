10eae9926080ad21b0361ac3d419808aa26cc65e
AndyEngland521 <andrew.england@sparkfun.com>
2020-04-13 11:27:47 -0600

    updating conf.py for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, SparkFun Electronics'
 author = 'SparkFun Electronics'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.01'
+release = '0.0.2'
 
 
 # -- General configuration ---------------------------------------------------
